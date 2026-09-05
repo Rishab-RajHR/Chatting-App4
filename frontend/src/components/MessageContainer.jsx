06285c399cbecd818d270e1fbf5390dba6dfc715
Rishab Raj <rr0948555@gmail.com>
2026-09-05 22:47:04 +0530

    Chat App

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -8,6 +8,9 @@ import { setSelectedUser } from '../redux/userSlice';
 const MessageContainer = () => {
     const {selectedUser} = useSelector(store=>store.user);
     const dispatch = useDispatch();
+
+    const isOnline = setOnlineUsers.includes(selectedUser._id);
+
     useEffect(() => {
         return () => dispatch(setSelectedUser(null));
     },[]);
@@ -17,7 +20,7 @@ const MessageContainer = () => {
         selectedUser !== null  ? (
        <div className='md:min-w-[550px] flex flex-col'>    
         <div className='flex gap-2 items-center bg-zinc-800 text-white px-4 py-2 mb-2'>
-           <div className='avatar online'>
+           <div className={`avatar ${isOnline ? 'online' : ''}`}>
                <div className='w-12 rounded-full'>
                    <img src={selectedUser?.profilePhoto} alt='user-profile'
                    />
